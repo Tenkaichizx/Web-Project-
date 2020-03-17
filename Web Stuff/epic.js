cb7b5ff70467bdbe6e73431d63b21859455749f6
Tenkaichizx <62303927+Tenkaichizx@users.noreply.github.com>
2020-03-17 17:33:37 -0400

    Ichi

--- Web Stuff/epic.js
+++ Web Stuff/epic.js
@@ -1,3 +1,13 @@
 function epic() {
     document.getElementById('demo').innerHTML = Date();
 }
+
+function woah() {
+    var d = new Date();
+    document.getElementById('noodle').innerHTML = d.getFullYear();
+}
+
+function emote() {
+   document.getElementById('happy').innerHTML= "&#x1F34E; &#x1F953; &#x1F32F; &#x1F95E;"       
+
+}
