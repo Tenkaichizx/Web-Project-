f69c2dfff026be7b42addcc889fee55ef1c95510
Tenkaichizx <62303927+Tenkaichizx@users.noreply.github.com>
2020-04-06 01:42:20 -0400

    This is epic

--- Web Stuff/script.js
+++ Web Stuff/script.js
@@ -1,15 +1,35 @@
+
+
 function epic() {
     document.getElementById('demo').innerHTML = Date();
+    var x = document.getElementById("first");
+    if(x.style.display=="block"){
+        x.style.display="none"
+      } else {
+        x.style.display="block"
+      }
+    
 }
 
 function woah() {
     var d = new Date();
     document.getElementById('noodle').innerHTML = d.getFullYear();
+    var x = document.getElementById("second");
+    if(x.style.display=="block"){
+        x.style.display="none"
+      } else {
+        x.style.display="block"
+      }
 }
 
 function emote() {
    document.getElementById('happy').innerHTML= "&#x1F34E; &#x1F953; &#x1F32F; &#x1F95E;"       
-
+   var x = document.getElementById("third");
+   if(x.style.display=="block"){
+       x.style.display="none"
+     } else {
+       x.style.display="block"
+     }
 }
 
 const secDiv = document.getElementById('sec');
